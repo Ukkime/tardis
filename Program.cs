8148512713d320d61e5e243d991bcd4a91bb5d4e
Ukkime <alexlatorre@me.com>
2024-01-11 21:15:36 +0100

    Program switch for UDP or SERVER mode by config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using ui;
 using System;
 using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace tardis;
 
@@ -9,8 +11,30 @@ class Program
     [STAThread]
     static void Main()
     {
+        var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("config.json", optional: true, reloadOnChange: true);
+
+        IConfiguration _config = builder.Build();
+
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Commands.StartTardis().Run());
+
+        // Ejecutamos la modalidad tardis que queremos
+
+        switch(_config["GeneralSettings:neighborMode"]) {
+            case "UDP":
+                Application.Run(new Commands.StartUDPTardis(_config).Run());
+                break;
+            case "SERVER":
+                Application.Run(new Commands.StartClientTardis(_config).Run());
+                break;
+            default:
+                System.Windows.Forms.MessageBox.Show("No se reconoce el modo definido en el config.json, por favor, revisa la configuración.");
+                Application.Exit();
+                break;
+        }
+
+        
     }
 }
