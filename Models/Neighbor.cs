4378291fa68135aa3db8ff38a5afb87aebd91da6
Ukkime <alexlatorre@me.com>
2024-01-18 09:11:18 +0100

    New Models for Json parse

---
+++ Models/Neighbor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using tardis.Models;
+
+public class Neighbor
+{
+    public string Name { get; set; }
+    public int NodeCount { get; set; }
+    public string Id { get; set; }
+    public List<NeighborNode> NeighborNodes { get; set; }
+}
---
+++ Models/NeighborNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tardis.Models
+{
+    public class NeighborNode
+    {
+        public string Name { get; set; }
+        public DateTime LastCommunication { get; set; }
+        public string Status { get; set; }
+    }
+}
